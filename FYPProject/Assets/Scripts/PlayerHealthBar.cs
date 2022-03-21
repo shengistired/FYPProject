bb6f683ce170032e62b46549d09d2ba18e3fdb61
XenoLustre <101568475+XenoLustre@users.noreply.github.com>
2022-03-21 16:17:18 +0800

    Health bar

---
+++ FYPProject/Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+	public int maxHealth = 100;
+	public int currentHealth;
+	
+	public HealthBar healthBar;
+	
+	
+	void StartHealth()
+	{
+		currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+	}
+	
+	void UpdateHealth()
+	{
+	if (Input.GetKeyDown(KeyCode.F5))
+		{
+			TakeDamage(20);
+		}
+		
+	}
+	
+	void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+		
+		healthBar.SetHealth(currentHealth);
+	}
+	
+}
