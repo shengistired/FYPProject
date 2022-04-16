c0767c9d04be3897e5294efa67f3d501a760d71a
chenghoonn <hoonnumber14@gmail.com>
2022-04-17 02:14:35 +0800

    sheep spawn

---
+++ FYPProject/Assets/SheepAI.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepAI : MonoBehaviour
+{
+    public float walkSpeed;
+
+    [HideInInspector]
+    public bool mustPatrol;
+    private bool mustTurn;
+
+    public Rigidbody2D rb;
+    public Transform groundcheckPos;
+    public LayerMask groundLayer;
+    public Collider2D bodyCollider;
+    //public GameObject boom;
+
+
+    void Start()
+    {
+        mustPatrol = true;
+        Physics2D.IgnoreLayerCollision(7, 7, true);
+    }
+
+    void Update()
+    {
+        if (mustPatrol)
+        {
+            Patrol();
+        }
+
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPosition.y > Screen.height || screenPosition.y < 0)
+        {
+            Destroy(gameObject);
+            GameObject.Find("Spawn_Sheep").GetComponent<Spawn_Sheep>().animalMin -= 1;
+            Debug.Log("Invisible");
+        }
+
+        //mustPatrol = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        switch (col.gameObject.name)
+        {
+            case "Fireball(Clone)":
+                //Instantiate(boom, col.gameObject.transform.position, Quaternion.identity);
+                //gameObject.GetComponent<EnemyHealth>().TakeDamage(damage);
+                Destroy(gameObject);
+                Debug.Log("Killed sheep");
+                break;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (mustPatrol == true)
+        {
+            mustTurn = !Physics2D.OverlapCircle(groundcheckPos.position, 0.1f, groundLayer);
+            //Patrol();
+        }
+    }
+
+    //enemy move
+    void Patrol()
+    {
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+        if (mustTurn == true || bodyCollider.IsTouchingLayers(groundLayer))
+        {
+            Flip();
+        }
+    }
+
+    //enemy flip
+    void Flip()
+    {
+        //mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustTurn = false;
+    }
+}
---
+++ FYPProject/Assets/Spawn_Sheep.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Sheep : MonoBehaviour
+{
+    public GameObject animals;
+
+    private int rand;
+    private int randPosition;
+
+    public int animalMin, animalMax;
+    public LayerMask groundLayer;
+    public Transform player;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        player = GameObject.Find("Mage").transform;
+        InvokeRepeating("SpawnAnimals", 0f, 8f);
+    }
+
+    private void SpawnAnimals()
+    {
+        if (animalMin < animalMax)
+        {
+            bool spawnAllowed = false;
+            while (!spawnAllowed)
+            {
+                float height = GameObject.Find("Terrain").GetComponent<TerrainGeneration>().heightAddition;
+                float x = Random.Range(player.position.x - 15, player.position.x + 15);
+                Vector3 spawnPos = new Vector3(x, height + 10, 0.0f);
+
+                if ((spawnPos - player.position).magnitude < 10)
+                {
+                    continue;
+                }
+                else
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        GameObject food = Instantiate(animals, spawnPos, Quaternion.identity) as GameObject;
+                        ++animalMin;
+                        spawnAllowed = true;
+                    }
+
+                }
+            }
+        }
+    }
+}
